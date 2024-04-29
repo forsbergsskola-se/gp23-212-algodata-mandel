b982a1fd88be2cd980afea82481f5b04c78e9fb4
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-04-29 15:28:57 +0200

    Add lists to P2

--- P2IteratorPattern/Program.cs
+++ P2IteratorPattern/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+Create a List with 5 numbers: 1, 1, 2, 3, 5.
+Assign it to a Variable of Type IEnumerable.
+Use GetEnumerator() and a while-Loop to print all elements of the IEnumerable-Variable.
+*/
 
-Console.WriteLine("Hello, World!");
+List<int> numberList = new() { 1, 1, 2, 3, 5 };
+
+IEnumerable<int> numbers = numberList;
+var iterator = numbers.GetEnumerator();
+while (iterator.MoveNext())
+{
+    Console.WriteLine(iterator.Current);
+}
+
+/*
+Can you also add up all numbers of the List using IEnumerable and then print the sum?
+If not, then what do you need to change?
+*/
+var sum = 0;
+foreach (var number in numbers)
+{
+    sum += number;
+}
+Console.WriteLine($"Sum: {sum}");
+
+
