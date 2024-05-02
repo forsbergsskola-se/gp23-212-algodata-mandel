626dacad1c5abd287281e3e68f4771edf803b195
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-02 15:54:46 +0200

    Add custom exception for stack an queue

--- TurboCollections/TurboLinkedQueue.cs
+++ TurboCollections/TurboLinkedQueue.cs
@@ -49,17 +49,12 @@ public class TurboLinkedQueue<T> : ITurboQueue<T> {
             }
             current.Next = newNode;  
         }
-        
-        // This is a bit more complicated. You need to let the last Node in the Queue know who's next after him.
-        // No other choice but looping through your Nodes until you reach the end.
-        // You know that you've reached the end, if the current Node's Next Node is null.
-        // Then, you assign a new Node containing the value to the current node's Next field.
     }
 
     public T Peek()
     {
         if (FirstNode == null)
-            throw new Exception();
+            throw new EmptyQueueException();
         return FirstNode.Value;
     }
 
@@ -89,4 +84,9 @@ public class TurboLinkedQueue<T> : ITurboQueue<T> {
     {
         return GetEnumerator();
     }
+}
+
+public class EmptyQueueException : Exception
+{
+    public override string Message => "The Stack is empty. Operation not possible.";
 }
--- TurboCollections/TurboLinkedStack.cs
+++ TurboCollections/TurboLinkedStack.cs
@@ -24,7 +24,7 @@ public class TurboLinkedStack<T> : IEnumerable<T> {
     public T Peek()
     {
         if (LastNode == null)
-            throw new Exception();
+            throw new EmptyStackException();
         return LastNode.Value;
     }
 
@@ -67,63 +67,9 @@ public class TurboLinkedStack<T> : IEnumerable<T> {
     {
         return GetEnumerator();
     }
+}
 
-    /*
-    public IEnumerator<T> GetEnumerator() {
-        // This one is a bonus and a bit more difficult.
-        // You need to create a new class named Enumerator.
-        // You find the details below.
-        var enumerator = new Enumerator(LastNode);
-        return enumerator;
-    }
-    
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
-
-    class Enumerator : IEnumerator<T> {
-        private Node CurrentNode;
-        private Node FirstNode;
-        private Node _lastNode;
-
-        public Enumerator(Node lastNode) {
-            _lastNode = lastNode;
-        }
-
-        public bool MoveNext()
-        {
-            // if we don't have a current node, we start with the first node
-            if(CurrentNode == null){
-                CurrentNode = FirstNode;
-            } else
-            {
-                CurrentNode = CurrentNode.Previous;
-                // Assign the Current Node's Previous Node to be the Current Node.
-            }
-
-            return CurrentNode != null;
-            // Return, whether there is a CurrentNode. Else, we have reached the end of the Stack, there's no more Elements.
-        }
-
-        public T Current {
-            get{
-                throw new NotImplementedException();
-                // Return the Current Node's Value.
-            }
-        }
-
-        // This Boiler Plate is necessary to correctly implement `IEnumerable` interface.
-        object IEnumerator.Current => Current;
-
-        public void Reset() {
-            // Look at Move. How can you make sure that this Enumerator starts over again?
-        }
-
-        public void Dispose()
-        {
-            // This function is not needed right now.
-        }
-    }*/
-    
+public class EmptyStackException : Exception
+{
+    public override string Message => "The Stack is empty. Operation not possible.";
 }
