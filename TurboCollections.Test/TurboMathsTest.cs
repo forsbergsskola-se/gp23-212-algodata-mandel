4cc2ae36c7e7652bf5e9a9a2da819c6fc237c425
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-02 10:51:04 +0200

    Remove default tests in MathsTests

--- TurboCollections.Test/TurboMathsTest.cs
+++ TurboCollections.Test/TurboMathsTest.cs
@@ -1,19 +1,5 @@
 namespace TurboCollections.Test;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
-
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
-}
-
 public static class MathsTests
 {
     [Test]
