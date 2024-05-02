c4eefeffab9b68b6ccdb3f1ccfd2c643cd3b435c
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-02 10:51:42 +0200

    Add tests to all methods in TurboLinkedStack

--- TurboCollections.Test/TurboLinkedStackTests.cs
+++ TurboCollections.Test/TurboLinkedStackTests.cs
@@ -2,5 +2,58 @@ namespace TurboCollections.Test;
 
 public class TurboLinkedStackTests
 {
+    [Test]
+    public void TestStackPush()
+    {
+        var stack = new TurboLinkedStack<int>();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        
+        CollectionAssert.IsNotEmpty(stack);
+        CollectionAssert.AreEqual(new []{3,2,1}, stack);
+    }
+
+    [Test]
+    public void TestStackPeek()
+    {                                             
+        var stack = new TurboLinkedStack<int>();
+        stack.Push(4);
+        stack.Push(3);
+        
+        Assert.AreEqual(3, stack.Peek());
+    }
+
+    [Test]
+    public void TestStackPop()
+    {
+        var stack = new TurboLinkedStack<int>();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        CollectionAssert.AreEqual(new []{3,2,1}, stack);
+        stack.Pop();
+        CollectionAssert.AreEqual(new []{2,1}, stack);
+    }
     
+    [Test]
+    public void TestStackClear()
+    {
+        var stack = new TurboLinkedStack<int>();
+        stack.Push(33);
+        CollectionAssert.IsNotEmpty(stack);
+        stack.Clear();
+        CollectionAssert.IsEmpty(stack);
+    }
+    
+    [Test]
+    public void TestStackCount()
+    {
+        var stack = new TurboLinkedStack<int>();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        
+        Assert.That(stack.Count, Is.EqualTo(3));
+    }
 }
