115dbbc736b4fbe705104ec2689038b278c291f9
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-06 14:17:00 +0200

    Add SortTestBase class to TurboSort.Tests

--- TurboCollections.Test/TurboSort.Test.cs
+++ TurboCollections.Test/TurboSort.Test.cs
@@ -1,13 +1,45 @@
 namespace TurboCollections.Test;
 
 public static class TurboSortTest
-{
-    [Test]
-    public static void TestSelectionSort()
+{ 
+    public class SelectionSortTests : SortTestBase
     {
-        var list = new List<int>() {5, 2, 4, 6, 1, 3};
-        TurboSort.SelectionSort(list);
+        protected override void SortList(List<int> list)
+        {
+            TurboSort.SelectionSort(list);
+        }
+    }
+    
+    public class BubbleSortTests : SortTestBase
+    {
+        protected override void SortList(List<int> list)
+        {
+            TurboSort.BubbleSort(list);
+        }
+    }
+    
+}
+
+public abstract class SortTestBase
+{
+    // This will be implemented by the inheriting class that calls a specific sorting algorithm
+    protected abstract void SortList(List<int> list);
+    
+    // These test cases will be inherited by any class inheriting from this class
+    [TestCase(100), TestCase(10), TestCase(100000)]
+    public void TestWithRandomNumbers(int count)
+    {
+        var numbers = Enumerable.Repeat(0, count)
+            .Select(it => Random.Shared.Next())
+            .ToList();
+
+        var expected = numbers.OrderBy(it => it)
+            .ToArray();
         
-        CollectionAssert.AreEqual(new []{1, 2, 3, 4, 5, 6}, list);
+        // start measuring performance
+        SortList(numbers);
+        // stop measuring performance
+        
+        CollectionAssert.AreEqual(expected, numbers);
     }
 }
