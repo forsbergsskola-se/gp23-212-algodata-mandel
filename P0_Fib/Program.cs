c0a5d488b631ffe451bc9c32ac8dfc51814cc8c0
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-06 14:53:52 +0200

    Add fib recursive and fib iterative

--- P0_Fib/Program.cs
+++ P0_Fib/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+namespace P0_Fib;
 
-Console.WriteLine("Hello, World!");
+public class Fib
+{
+    public int GetFibRecursive(int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int result = GetFibRecursive(n - 1) + GetFibRecursive(n - 2);
+        return result;
+    }
+
+    public int GetFibIterative(int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int first = 0;
+        int second = 1;
+        
+        nextIteration:
+        int current = first + second;
+        first = second;
+        second = current;
+        if (n > 1)
+        {
+            goto nextIteration;
+        }
+
+        return current;
+    }
+}
