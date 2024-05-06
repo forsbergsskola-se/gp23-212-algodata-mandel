08a20c6e033589af5b1f27b12eb5cdec9bb820b5
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-06 15:13:07 +0200

    Add stopwatch to test fib times

--- P0_Fib/Program.cs
+++ P0_Fib/Program.cs
@@ -1,34 +1,64 @@
 ﻿
-namespace P0_Fib;
+using System.Diagnostics;
 
-public class Fib
+int GetFibRecursive(int n)
 {
-    public int GetFibRecursive(int n)
-    {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
+    if (n == 0) return 0;
+    if (n == 1) return 1;
 
-        int result = GetFibRecursive(n - 1) + GetFibRecursive(n - 2);
-        return result;
-    }
-
-    public int GetFibIterative(int n)
-    {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-
-        int first = 0;
-        int second = 1;
-        
-        nextIteration:
-        int current = first + second;
-        first = second;
-        second = current;
-        if (n > 1)
-        {
-            goto nextIteration;
-        }
-
-        return current;
-    }
+    int result = GetFibRecursive(n - 1) + GetFibRecursive(n - 2);
+    return result;
 }
+
+int GetFibIterative(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    int first = 0;
+    int second = 1;
+        
+    nextIteration:
+    int current = first + second;
+    first = second;
+    second = current;
+    n--;
+    if (n > 1)
+    {
+        goto nextIteration;
+    }
+    return current;
+}
+    
+void TestFibRecursive(int n)
+{
+    Stopwatch stopwatch = new Stopwatch();
+        
+    stopwatch.Start();
+    GetFibRecursive(n);
+    stopwatch.Stop();
+    
+    TimeSpan ts = stopwatch.Elapsed;
+    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+    Console.WriteLine("Fib Recursive took " + elapsedTime);
+}
+    
+void TestFibIterative(int n)
+{
+    Stopwatch stopwatch = new Stopwatch();
+        
+    stopwatch.Start();
+    GetFibIterative(n);
+    stopwatch.Stop();
+    
+    TimeSpan ts = stopwatch.Elapsed;
+    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+    Console.WriteLine("Fib Iterative took " + elapsedTime);
+}
+
+TestFibRecursive(40);
+TestFibIterative(40);
--- TurboCollections.Test/TurboMathsTest.cs
+++ TurboCollections.Test/TurboMathsTest.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace TurboCollections.Test;
 
 public static class MathsTests
