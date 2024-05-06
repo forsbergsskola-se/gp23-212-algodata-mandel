7a81f7e12dd9c288ba16bb789fe7b838010f327f
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-06 10:30:17 +0200

    First draft SelectionSort

--- TurboCollections/TurboSort.cs
+++ TurboCollections/TurboSort.cs
@@ -2,5 +2,24 @@ namespace TurboCollections;
 
 public class TurboSort
 {
-    
+    public void SelectionSort(List<int> list)
+    {
+        int min;
+        for (var i = 0; i < list.Count - 2; i++)
+        {
+            min = i;
+            for (var j = i + 1; j < list.Count -1 ; j++)
+            {
+                if (list[j] < list[min])
+                {
+                    min = j;
+                }
+            }
+
+            if (min != i)
+            {
+                (list[min], list[i]) = (list[i], list[min]);
+            }
+        }
+    }
 }
