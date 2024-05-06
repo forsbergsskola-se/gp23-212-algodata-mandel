feeee398d72951ec58d4ed9b61545883771169b1
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-06 12:15:59 +0200

    Update SelectionSort.Test

--- TurboCollections.Test/SelectionSort.Test.cs
+++ TurboCollections.Test/SelectionSort.Test.cs
@@ -6,14 +6,7 @@ public static class SelectionSort_Test
     [Test]
     public static void TestSelectionSort()
     {
-        var list = new List<int>();
-        CollectionAssert.IsEmpty(list);
-        list.Add(5);
-        list.Add(2);
-        list.Add(4);
-        list.Add(6);
-        list.Add(1);
-        list.Add(3);
+        var list = new List<int>() {5, 2, 4, 6, 1, 3};
         TurboSort.SelectionSort(list);
         
         CollectionAssert.AreEqual(new []{1, 2, 3, 4, 5, 6}, list);
