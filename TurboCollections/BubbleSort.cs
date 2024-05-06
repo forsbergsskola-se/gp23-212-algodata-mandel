c6b042b27ab19989625b031e2155c3f382546c5a
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-06 14:16:36 +0200

    Refactor BubbleSort Algorithm

--- TurboCollections/BubbleSort.cs
+++ TurboCollections/BubbleSort.cs
@@ -4,8 +4,21 @@ public static partial class TurboSort
 {
     public static void BubbleSort(List<int> list)
     {
+        var loop = list.Count;
         
-        
+        for (var i = loop-1; i > 0; i--)
+        {
+            var swapped = false;
+            for (var j = 0; j < i; j++)
+            {
+                if (list[j] > list[j + 1])
+                {
+                    (list[j], list[j + 1]) = (list[j + 1], list[j]);
+                    swapped = true;
+                }
+            }
+            if(!swapped) break;
+        }
     }
     
 }
