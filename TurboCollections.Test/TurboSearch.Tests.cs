59c8eaf7c77ac7a2332431120537150f7e964902
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-08 12:27:59 +0200

    Add test for BinarySearch List<int>

--- TurboCollections.Test/TurboSearch.Tests.cs
+++ TurboCollections.Test/TurboSearch.Tests.cs
@@ -3,13 +3,21 @@ namespace TurboCollections.Test;
 public class TurboSearchTests
 {
     [Test]
-    public void LinearSearchTest()
+    public void LinearSearchIntTest()
     {
         var list = new List<int>() { 1, 2, 3, 4, 5, 6 };
         
         Assert.That(TurboSearch.LinearSearch(list, 6), Is.EqualTo(5));
         Assert.That(TurboSearch.LinearSearch(list, 8), Is.EqualTo(-1));
     }
-    
+
+    [Test]
+    public void BinarySearchIntTest()
+    {
+        var list = new List<int>() { 1, 2, 3, 4, 5, 6 };
+        
+        Assert.That(TurboSearch.BinarySearch(list, 3), Is.EqualTo(2));
+        Assert.That(TurboSearch.BinarySearch(list, 8), Is.EqualTo(-1));
+    }
     
 }
