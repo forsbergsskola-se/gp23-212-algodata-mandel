f2dc8d34b7d2effb2c5bcf127a57afed3929e3cd
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-08 14:27:19 +0200

    Add drafts of generic linear and binary search methods

--- TurboCollections/TurboSearch.cs
+++ TurboCollections/TurboSearch.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 namespace TurboCollections;
 
-public class TurboSearch
+public static class TurboSearch
 {
     public static int LinearSearch(List<int> list, int value)
     {
@@ -31,6 +31,43 @@ public class TurboSearch
             else if (list[mid] > value)
                 upperBound = mid - 1;
             else if (list[mid] == value)
+                return mid;
+        }
+
+        return -1;
+    }
+    
+    
+    // Generics
+    
+    public static int GenericLinearSearch<T>(IEnumerable<T> list, T value)
+    {
+        int index = 0;
+        foreach (var item in list)
+        {
+            if (item == null && value == null || item.Equals(value))
+            {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+    
+    public static int GenericBinarySearch<T>(List<T> list, T value) where T : IComparable<T>
+    {
+        var lowerBound = 0;
+        var upperBound = list.Count - 1;
+
+        while (lowerBound <= upperBound)
+        {
+            var mid = (lowerBound + upperBound) / 2;
+
+            if (list.ElementAt(mid).CompareTo(value) < 0)
+                lowerBound = mid + 1;
+            else if (list.ElementAt(mid).CompareTo(value) > 0)
+                upperBound = mid - 1;
+            else if (list.ElementAt(mid).CompareTo(value) == 0)
                 return mid;
         }
 
