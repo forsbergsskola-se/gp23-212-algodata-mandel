858b63956915db72711b596f67cbdf03a428a5fc
mandel <mandel.cohen@student.forsbergsskola.se>
2024-05-14 14:52:49 +0200

    Debug test on delete, method returns true or false but doesn't delete properly

--- TurboCollections.Test/TurboBst.Tests.cs
+++ TurboCollections.Test/TurboBst.Tests.cs
@@ -31,7 +31,37 @@ public class TurboBstTests
         Assert.That(tree.Search(9), Is.EqualTo(false));
         
     }
-    
-    
-    
+
+    [Test]
+    public void TestTraversInOrderBst()
+    {
+        var tree = new TurboBinarySearchTree<int>();
+
+        tree.Insert(4);
+        tree.Insert(2);
+        tree.Insert(6);
+        tree.Insert(1);
+        tree.Insert(3);
+        tree.Insert(5);
+        tree.Insert(7);
+
+        CollectionAssert.AreEqual(new []{1,2,3,4,5,6,7}, tree);
+    }
+
+    [Test]
+    public void TestDeleteValueFromTree()
+    {
+        var tree = new TurboBinarySearchTree<int>();
+
+        tree.Insert(4);
+        tree.Insert(2);
+        tree.Insert(6);
+        tree.Insert(1);
+        tree.Insert(3);
+        tree.Insert(5);
+        tree.Insert(7);
+        
+        Assert.That(tree.Delete(5), Is.EqualTo(true));
+        CollectionAssert.AreEqual(new []{1,2,3,4,6,7}, tree);
+    }
 }
