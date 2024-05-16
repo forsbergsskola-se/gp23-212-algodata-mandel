0df61e379f0f959764562461500795e47bfb63ed
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-16 13:36:58 +0200

    Run test to compare trees and balance

--- TurboCollections.Test/TurboAVL.Tests.cs
+++ TurboCollections.Test/TurboAVL.Tests.cs
@@ -2,5 +2,18 @@ namespace TurboCollections.Test;
 
 public class TurboAVL_Tests
 {
-    
+    [Test]
+    public void TestInsertAvl()
+    {
+        var tree = new AVLTree();
+        tree.root = tree.Insert(tree.root, 10);  
+        tree.root = tree.Insert(tree.root, 20);  
+        tree.root = tree.Insert(tree.root, 30);  
+        tree.root = tree.Insert(tree.root, 40);  
+        tree.root = tree.Insert(tree.root, 50);  
+        tree.root = tree.Insert(tree.root, 25);  
+        
+        
+        Assert.Pass();
+    }
 }
