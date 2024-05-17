ada33ed9351bfec5cc1cd6c5b30aa670a99ad7fd
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-17 15:46:32 +0200

    Add remove at index function

--- TurboCollections/TurboLinkedList.cs
+++ TurboCollections/TurboLinkedList.cs
@@ -6,7 +6,7 @@ public class TurboLinkedList<T> : IEnumerable<T>
 {
     class Node {
         public T Value;
-        public Node Next;
+        public Node? Next;
 
         public Node(T value)
         {
@@ -15,7 +15,7 @@ public class TurboLinkedList<T> : IEnumerable<T>
         }
     }
 
-    private Node first;
+    private Node? first;
 
     public void Add(T value)
     {
@@ -32,8 +32,72 @@ public class TurboLinkedList<T> : IEnumerable<T>
         current.Next = new Node(value);
     }
     
-    // Remove
-    // Remove at index
+    public int Count 
+    {
+        get
+        {
+            int count = 0;
+            var current = first;
+            while (current != null)
+            {
+                count++;
+                current = current.Next;
+            }
+            return count;
+        }
+    }
+
+    public void Remove(T value)
+    {
+        var current = first;
+
+        if (current.Value.Equals(value))
+        {
+            first = first.Next;
+            return;
+        }
+        while (current.Next != null)
+        {
+            if (current.Next.Value.Equals(value))
+            {
+                current.Next = current.Next.Next;
+            }
+            current = current.Next;
+        }
+    }
+
+    public void RemoveAtIndex(int index)
+    {
+        var current = first;
+        int currentIndex = 0;
+        
+        if (Count < index)
+            return;
+        if (currentIndex == index)
+        {
+            first = current.Next;
+            return;
+        }
+
+        while (current != null)
+        {
+            currentIndex++;
+            if (currentIndex == index)
+            {
+                current.Next = current.Next.Next;
+            }
+
+            current = current.Next;
+        }
+        
+    }
+
+    public void Clear()
+    {
+        first = null;
+    }
+    
+    
     // Clear/delete
     // Count
     // Count a value
