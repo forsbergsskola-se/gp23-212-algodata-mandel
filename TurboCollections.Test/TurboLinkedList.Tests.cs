3c3c55f29bc8e6b1645830bb4e9018a7b3d0287e
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-17 14:27:36 +0200

    Add tests for linked list

--- TurboCollections.Test/TurboLinkedList.Tests.cs
+++ TurboCollections.Test/TurboLinkedList.Tests.cs
@@ -2,5 +2,19 @@ namespace TurboCollections.Test;
 
 public class TurboLinkedList_Tests
 {
-    
+    [Test]
+    public void TestAddToList()
+    {
+        var list = new TurboLinkedList<int>();
+        
+        list.Add(1);
+        list.Add(3);
+        list.Add(5);
+        
+        foreach (var i in list)
+        {
+            Console.Write(i);
+        }
+        Assert.That(list, Is.EqualTo(new []{1,3,5}));
+    }
 }
