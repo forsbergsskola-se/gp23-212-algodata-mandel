9e97c9d93aacb930f2e83fdf04bf91d9e807f183
mandelcohen <mandelcohen@users.noreply.github.com>
2024-10-14 07:48:21 +0000

    deploy: 5b19f2e758c1668a33eeb4838347e35da708f459

--- search/all_b.js
+++ search/all_b.js
@@ -3,14 +3,13 @@ var searchData=
   ['test_20case_2010_0',['Test Case (10)',['../index.html#autotoc_md2',1,'']]],
   ['test_20case_20100_1',['Test Case (100)',['../index.html#autotoc_md3',1,'']]],
   ['test_20case_20100000_2',['Test Case (100000)',['../index.html#autotoc_md4',1,'']]],
-  ['turboavl_5ftests_3',['TurboAVL_Tests',['../class_turbo_collections_1_1_test_1_1_turbo_a_v_l___tests.html',1,'TurboCollections::Test']]],
-  ['turbobinarysearchtree_4',['TurboBinarySearchTree',['../class_turbo_collections_1_1_turbo_binary_search_tree.html',1,'TurboCollections']]],
-  ['turbobsttests_5',['TurboBstTests',['../class_turbo_collections_1_1_test_1_1_turbo_bst_tests.html',1,'TurboCollections::Test']]],
-  ['turbocollections_6',['TurboCollections',['../namespace_turbo_collections.html',1,'']]],
-  ['turbocollections_3a_3atest_7',['Test',['../namespace_turbo_collections_1_1_test.html',1,'TurboCollections']]],
-  ['turbolinkedlist_8',['TurboLinkedList',['../class_turbo_collections_1_1_turbo_linked_list.html',1,'TurboCollections']]],
-  ['turbolinkedlist_5ftests_9',['TurboLinkedList_Tests',['../class_turbo_collections_1_1_test_1_1_turbo_linked_list___tests.html',1,'TurboCollections::Test']]],
-  ['turbolinkedqueue_10',['TurboLinkedQueue',['../class_turbo_collections_1_1_turbo_linked_queue.html',1,'TurboCollections']]],
-  ['turbolinkedstack_11',['TurboLinkedStack',['../class_turbo_collections_1_1_turbo_linked_stack.html',1,'TurboCollections']]],
-  ['turbosearchtests_12',['TurboSearchTests',['../class_turbo_collections_1_1_test_1_1_turbo_search_tests.html',1,'TurboCollections::Test']]]
+  ['turbobinarysearchtree_3',['TurboBinarySearchTree',['../class_turbo_collections_1_1_turbo_binary_search_tree.html',1,'TurboCollections']]],
+  ['turbobsttests_4',['TurboBstTests',['../class_turbo_collections_1_1_test_1_1_turbo_bst_tests.html',1,'TurboCollections::Test']]],
+  ['turbocollections_5',['TurboCollections',['../namespace_turbo_collections.html',1,'']]],
+  ['turbocollections_3a_3atest_6',['Test',['../namespace_turbo_collections_1_1_test.html',1,'TurboCollections']]],
+  ['turbolinkedlist_7',['TurboLinkedList',['../class_turbo_collections_1_1_turbo_linked_list.html',1,'TurboCollections']]],
+  ['turbolinkedlist_5ftests_8',['TurboLinkedList_Tests',['../class_turbo_collections_1_1_test_1_1_turbo_linked_list___tests.html',1,'TurboCollections::Test']]],
+  ['turbolinkedqueue_9',['TurboLinkedQueue',['../class_turbo_collections_1_1_turbo_linked_queue.html',1,'TurboCollections']]],
+  ['turbolinkedstack_10',['TurboLinkedStack',['../class_turbo_collections_1_1_turbo_linked_stack.html',1,'TurboCollections']]],
+  ['turbosearchtests_11',['TurboSearchTests',['../class_turbo_collections_1_1_test_1_1_turbo_search_tests.html',1,'TurboCollections::Test']]]
 ];
